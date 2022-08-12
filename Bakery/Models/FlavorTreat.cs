5d5efc41873454e5a7220de100ae3bee32c77696
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-08-12 14:15:09 -0700

    Updated everything to treats and flavors

--- Bakery/Models/FlavorTreat.cs
+++ Bakery/Models/FlavorTreat.cs
@@ -2,7 +2,7 @@ namespace Bakery.Models
 {
   public class FlavorTreat
   {       
-    public int CategoryRecipeId { get; set; }
+    public int FlavorTreatId { get; set; }
     public int TreatId { get; set; }
     public int FlavorId { get; set; }
     public virtual Treat Treat { get; set; }
