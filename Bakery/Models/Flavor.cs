821cf9e000476a365b6438b56a334625aaa7fb2d
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-08-12 13:12:04 -0700

    added flavor model

---
+++ Bakery/Models/Flavor.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Flavor
+  {
+    public Flavor()
+    {
+      this.JoinEntities = new HashSet<FlavorTreat>();
+    }
+
+    public int FlavorId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
+  }
+}
